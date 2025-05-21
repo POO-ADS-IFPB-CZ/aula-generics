459558d6b2f0f8180ae46771a78908b413ed27bc
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-05-21 08:21:31 -0300

    Utilizando o cadastro genérico

--- src/App.java
+++ src/App.java
@@ -2,13 +2,8 @@ import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) {
-        CadastroAluno cadastroAluno = new CadastroAluno();
-        cadastroAluno.adicionarAluno(
-            new Aluno("111.111.111-01", "João", 123)
-        );
-        cadastroAluno.adicionarAluno(
-            new Aluno("222.222.222-02", "Maria", 456)
-        );
-        System.out.println(Arrays.toString(cadastroAluno.getAlunos()));
+        Cadastro<Aluno> cadastroAluno = new Cadastro<>();
+        Cadastro<Professor> cadastroProf = new Cadastro<>();
+        Cadastro<String> cadastroString = new Cadastro<>();
     }
 }
