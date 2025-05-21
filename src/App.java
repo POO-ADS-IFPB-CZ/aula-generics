b287894e8ef735344ec1c2b5936d9c5b76d490c9
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-05-21 08:39:52 -0300

    Exemplo de wildcard

--- src/App.java
+++ src/App.java
@@ -3,9 +3,8 @@ import java.util.Objects;
 
 public class App {
     public static void main(String[] args) {
-        teste("João");
-        teste(1);
-        teste(new Professor("111.111.111-01", "JOão", 1230));
+        Cadastro<? super Integer> cadastro;
+        cadastro = new Cadastro<Number>();
     }
 
     public static <T extends Number> void teste(T obj){
