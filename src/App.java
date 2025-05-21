4a26137e4ba78bf81ea75b6bad4712d613f97412
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-05-21 08:34:14 -0300

    Exemplo de método genérico

--- src/App.java
+++ src/App.java
@@ -1,9 +1,15 @@
 import java.util.Arrays;
+import java.util.Objects;
 
 public class App {
     public static void main(String[] args) {
-        Cadastro<Aluno> cadastroAluno = new Cadastro<>();
-        Cadastro<Professor> cadastroProf = new Cadastro<>();
-        Cadastro<String> cadastroString = new Cadastro<>();
+        teste("João");
+        teste(1);
+        teste(new Professor("111.111.111-01", "JOão", 1230));
     }
+
+    public static <T extends Number> void teste(T obj){
+
+    }
+
 }
